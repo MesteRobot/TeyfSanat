c1bf3f3270826e7bebe603a75c55695cebb4ec73
MesteRobot <hassan.doodiyan@gmail.com>
2021-04-23 18:41:59 +0430

    start app database & set the settings to migrate and create database

--- TeyfSanat/settings.py
+++ TeyfSanat/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'database',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'TeyfSanat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TeyfSanat',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': '1991',
+        'PORT': '5432',
     }
 }
 
